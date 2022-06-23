04c8b7c559b7169036f7bfabc8ebd45e63a1f170
Chigozirim <iamokwukwe@gmail.com>
2022-06-23 22:56:31 +0100

    Align code to PEP8 standard

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,7 @@ def canUnlockAll(boxes):
     """Problem: You have n number of locked boxes in front of you. Each box is
     numbered sequentially from 0 to n - 1 and each box may contain keys to the
     other boxes.
-    
+
     - boxes is a list of lists
     - A key with the same number as a box opens that box
     - You can assume all keys will be positive integers
