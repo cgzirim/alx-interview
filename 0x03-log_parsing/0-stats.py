dd57280de5634a8b33767bc477c56e151be743d4
Chigozirim <iamokwukwe@gmail.com>
2022-07-10 16:39:15 +0100

    Align code to PEP8 standard

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics."""
-from re import S
+"""log parsing"""
 import sys
-from traceback import print_stack
 
 
-if __name__ == "__main__":
+def print_data(total_file_size, status_code_data):
+    """prints total size and status code count"""
+    print('File size: {}'.format(total_file_size))
+    for k, v in sorted(status_code_data.items()):
+        if v != 0:
+            print('{}: {}'.format(k, v))
 
-    file_size, count = 0, 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
 
-    def print_stat(status_codes, file_size):
-        """Prints File size and status code count."""
-        print("File size: {}".format(file_size))
-        for k, v in status_codes.items():
-            if v != 0:
-                print("{}: {}".format(k, v))
-
-    try:
-        for line in sys.stdin:
-            data = line.split(" ")
-            try:
-                file_size += int(data[-1])
-                if data[-2] in status_codes.keys():
-                    status_codes[data[-2]] += 1
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_code_data = {code: 0 for code in status_codes}
+total_file_size = 0
+try:
+    count = 0
+    for line in sys.stdin:
+        splitstr = line.split()
+        try:
+            total_file_size += int(splitstr[-1])
+            code = splitstr[-2]
+            if code in status_code_data:
                 count += 1
-            except (IndexError, ValueError):
-                pass
-
-            if (count % 10) == 0:
-                print_stat(status_codes, file_size)
-        print_stat(status_codes, file_size)
-    except KeyboardInterrupt:
-        print_stat(status_codes, file_size)
-        raise
+                status_code_data[code] += 1
+                if count % 10 == 0:
+                    print_data(total_file_size, status_code_data)
+        except:
+            pass
+except KeyboardInterrupt:
+    print_data(total_file_size, status_code_data)
+    raise
+else:
+    print_data(total_file_size, status_code_data)
