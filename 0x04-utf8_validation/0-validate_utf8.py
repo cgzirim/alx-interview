3e3ce5facdc26f884dfc45d8966c93c30aeba32b
Chigozirim <iamokwukwe@gmail.com>
2022-07-14 22:18:07 +0100

    Solve problem without importing a module

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """Defines a function named validUTF8."""
-import chardet
-from typing import List
 
 
-def validUTF8(data: List) -> bool:
+def validUTF8(data):
     """Returns True if a given data set represents a valid UTF-8 encoding.
     Otherwise, return False.
     """
     for integer in data:
-        result = chardet.detect(chr(integer).encode("utf-8"))
-        encoding = result["encoding"]
-        if encoding != "ascii":
+        if integer > 255 or elem < 0:
             return False
     return True
