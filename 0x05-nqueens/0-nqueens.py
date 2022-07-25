fa6f889df186ec1800f955af5c745c78103dfb3a
Chigozirim <iamokwukwe@gmail.com>
2022-07-25 13:18:47 +0100

    Solve backtracking problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
 
-n = int(argv[1])
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
 
 def validate(chess_board: dict, queen_key: int) -> bool:
