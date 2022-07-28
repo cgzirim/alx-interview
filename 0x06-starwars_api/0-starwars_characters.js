2a54a58d36c71007b497e9c5ef6c3e2fe2d17d66
Chigozirim <iamokwukwe@gmail.com>
2022-07-28 15:58:50 +0100

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 
-const id = process.argv[2];
+if (process.argv.length > 2) {
+  const id = process.argv[2];
+  const request = require('request');
+  const API_URL = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-request(url, (err, resp, body) => {
-  if (err) console.log(err);
-  const characters = JSON.parse(body).characters;
+  request(API_URL, (err, resp, body) => {
+    if (err) console.log(err);
+    const charactersURL = JSON.parse(body).characters;
 
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], (err, resp, body) => {
-      if (err) console.log(err);
-      console.log(JSON.parse(body).name);
-    });
-  }
-});
+    for (let i = 0; i < charactersURL.length; i++) {
+      request(charactersURL[i], (err, resp, body) => {
+        if (err) console.log(err);
+        console.log(JSON.parse(body).name);
+      });
+    }
+  });
+}
