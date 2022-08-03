b890939b45d350964cbb12fdec54a3aa8bf71f8d
Chigozirim <iamokwukwe@gmail.com>
2022-08-03 11:32:11 +0100

    Make function untyped

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Rotate 2D Matrix """
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List]) -> List[List]:
+def rotate_2d_matrix(matrix):
     """Rotates a 2D matrix by 90 degrees clockwise."""
     org_matrix = matrix
     rotated_matrix = []
