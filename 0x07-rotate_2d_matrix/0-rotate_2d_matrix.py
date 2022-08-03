2579c00172c606f4dcd7d17ccf7851dd5a37a70b
Chigozirim <iamokwukwe@gmail.com>
2022-08-03 11:30:26 +0100

    Initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix """
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List]) -> List[List]:
+    """Rotates a 2D matrix by 90 degrees clockwise."""
+    org_matrix = matrix
+    rotated_matrix = []
+
+    i = 0
+    while i < len(org_matrix):
+        new_row = []
+
+        for row in org_matrix:
+            new_row.append(row[i])
+        new_row.reverse()
+        rotated_matrix.append(new_row)
+
+        i += 1
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix[i][j] = rotated_matrix[i][j]
