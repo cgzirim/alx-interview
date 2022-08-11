59023f09c7870f53ff70aba16b8d5681bdfcb5ed
Chigozirim <iamokwukwe@gmail.com>
2022-08-11 14:19:18 +0100

    Solve the make change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the function makeChange."""
+
+
+def makeChange(coins, total):
+    """Returns fewest number of coins needed to meet total.
+    Parameteres:
+        - coins: a  list of values of coins in your possession
+        - total: given amount to meet.
+    """
+    if not total or not coins or total == 0:
+        return 0
+
+    for coin in sorted(coins, reverse=True):
+        for cn in sorted(coins, reverse=True):
+            sum_ = coin
+            count = 1
+            while sum_ <= total + 1:
+                if sum_ == total:
+                    return count
+                sum_ += cn
+                count += 1
+    return -1
